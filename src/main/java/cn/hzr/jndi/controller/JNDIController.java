f2a3a74556f1ae5c357d047fcd3e8d46513b7e41
hezhiren <hezhiren1994@163.com>
2018-11-30 11:29:25 +0800

    修复返回String字符串，中文编码错误的问题

--- src/main/java/cn/hzr/jndi/controller/JNDIController.java
+++ src/main/java/cn/hzr/jndi/controller/JNDIController.java
@@ -2,7 +2,6 @@ package cn.hzr.jndi.controller;
 
 import cn.hzr.jndi.dao.UserDao;
 import cn.hzr.jndi.entity.User;
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,13 +19,33 @@ public class JNDIController {
     @Autowired
     private UserDao userDao;
 
+    /**
+     * 返回String字符串，会存在中文编码问题，需要在spring-mvc.xml配合字符集
+     * @param name
+     * @return
+     * @throws Exception
+     */
     @RequestMapping(value = "/testConnect.do/{name}")
     @ResponseBody
-    public JSONObject testConnect(@PathVariable String name) throws Exception{
+    public String testConnect(@PathVariable String name) throws Exception{
+        User user = userDao.findUserByName(name);
+        return user.toString();
+    }
+
+    /**
+     * json 数据格式返回的中文编码是正常的
+     * @param name
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/testJson.do/{name}")
+    @ResponseBody
+    public JSONObject testJson(@PathVariable String name) throws Exception{
         User user = userDao.findUserByName(name);
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("user", user);
         return jsonObject;
     }
+
 
 }
